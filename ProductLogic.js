26d116ffc90f2bce0257832a03483ef9f99ca3f6
sankhadeep <sankhadeep2007@gmail.com>
2020-07-29 19:11:04 +0530

    Update ProductLogic.js
    added methods

--- ProductLogic.js
+++ ProductLogic.js
@@ -1,35 +1,81 @@
-function ProductLogic() {
-    // private scoped to the function
-	//ProductRowId, ProductId, ProductName, CategoryName, Manufacturer, Description, BasePrice
-    var products = [
-        { ProductRowId: 101, ProductId: 'P1', ProductName: 'p1',CategoryName:"C1",Manufacturer:"m1",Description:"D1",BasePrice:'BasePrice1'},
-         { ProductRowId: 102, ProductId: 'P1', ProductName: 'p1',CategoryName:"C2",Manufacturer:"m2",Description:"D3",BasePrice:'BasePrice2'},
-		  { ProductRowId: 103, ProductId: 'P1', ProductName: 'p1',CategoryName:"C3",Manufacturer:"m3",Description:"D2",BasePrice:'BasePrice3'},
-		   { ProductRowId: 104, ProductId: 'P1', ProductName: 'p1',CategoryName:"C4",Manufacturer:"m4",Description:"D4",BasePrice:'BasePrice4'},
-		    { ProductRowId: 105, ProductId: 'P1', ProductName: 'p1',CategoryName:"C5",Manufacturer:"m5",Description:"D5",BasePrice:'BasePrice5'},
-    ];
-    // returning an Object
-    return {
-        getPorducts: function() {
-            return products;
-        },
-		getCategory:function(){
-			var arrCat=[];
-			for(p in products ){
-			   arrCat.push(products[p].CategoryName);
+function page_onload(){
+	       var pObj = new ProductLogic();
+		   arrProd=pObj.getPorducts();
+		   arrCat=pObj.getCategory();
+		   arrMan=pObj.getManufacture();
+		    selectOptCat(arrCat);
+			selectOptMan(arrMan);
+            gnerateTable(arrProd);
+			
+			///Generate options
+			function selectOptCat(arrCat){
+				var slStr//="<select id='selectCat' onchange='onClickSelect()'>";
+				for (var i=0; i <arrCat.length; i++)
+				{
+				slStr+="<option>" + arrCat[i] + "</option>";
+				}
+                slStr+="</select>"
+               document.getElementById('selectCat').innerHTML = slStr;
 			}
-			return arrCat;
-		},
-		getManufacture:function(){
-			var arrManu=[];
-			for(p in products ){
-			   arrManu.push(products[p].Manufacturer);
+			
+			function selectOptMan(arrMan){
+				var slStr//="<select id='selectMan' onchange='onClickSelect()'>";
+				for (var i=0; i <arrMan.length; i++)
+				{
+				slStr+="<option>" + arrMan[i] + "</option>";
+				}
+                slStr+="</select>"
+               document.getElementById('selectMan').innerHTML = slStr;
 			}
-			return arrManu;
-		},
-        addProduct: function(prd) {
-            products.push(prd);
-            return products;
-        }
-    };
+            // a function to generate table
+            function gnerateTable(arrProd) {
+               
+				var keys1=Object.keys(arrProd[0]);
+				var thsrt='<thead>';
+				 thsrt += '<tr>' 
+				 for(var j = 0; j < keys1.length; j++)
+				 {
+                    thsrt +=  '<th>'+keys1[j]+ '</th>';// + arrProd[keys] + '</td><td>' + arrProd[i] + '</td></tr>';
+			      }
+				  thsrt += '</tr></th></thead>';
+				 // document.getElementById('tbody').innerHTML = thsrt;
+				 var tr = '';
+				 tr=thsrt;
+                for (var i = 0; i < arrProd.length; i++) {
+				var keys=Object.keys(arrProd[i]);
+				   tr += '<tr>'; 
+				   for(var j = 0; j < keys.length; j++){
+                    tr +=  '<td>'+arrProd[i][keys[j]];
+					}
+					tr+= '</td>'+'<td><input type="button" id="delPOIbutton" value="Delete" onclick="deleteRow(this)"/></td></tr>';
+					//tr +=;
+					document.getElementById('tbody').innerHTML = tr;
+                }
+				
+                
+            }
+			
+			document.getElementById('btnCreate').addEventListener('click', function() {
+                var prod = { ProductRowId: 110, ProductId: 'P1', ProductName: 'p1',CategoryName:"C1",Manufacturer:"m1",Description:"D1",BasePrice:'BasePrice1'};
+				pObj.addProduct(prod);
+				 arrProd=pObj.getPorducts();
+				arrCat=pObj.getCategory();
+				arrMan=pObj.getManufacture();
+				selectOptCat(arrCat);
+				selectOptMan(arrMan);
+				gnerateTable(arrProd);
+                //gnerateTable(arrEmps);
+
+            }, false);
+			
+			document.getElementById('btnUpdate').addEventListener('click', function() {
+                arrEmps.push({
+                    EmpNo: 5,
+                    EmpName: 'Prashant',
+                    Salary: 4555
+                });
+                console.log(JSON.stringify(arrEmps));
+                gnerateTable(arrEmps);
+
+            }, false);
 }
