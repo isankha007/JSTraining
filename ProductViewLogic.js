ce6d7932be088f81a1ea225649ce25d4bd1ec4c3
sankhadeep <sankhadeep2007@gmail.com>
2020-07-29 17:13:50 +0530

    Update ProductViewLogic.js
    added new methods

--- ProductViewLogic.js
+++ ProductViewLogic.js
@@ -1,37 +1,31 @@
 function page_onload(){
 	 var pObj = new ProductLogic();
 		   arrProd=pObj.getPorducts();
-		    selectOptCat(pObj.getCategory());
+		   arrCat=pObj.getCategory();
+		   arrMan=pObj.getManufacture();
+		    selectOptCat(arrCat);
+			selectOptMan(arrMan);
             gnerateTable(arrProd);
 			
 			///Generate options
-			/*function selectOpt(){
-				var limitOfRows=16;
-				var positions = 0;
+			function selectOptCat(arrCat){
 				var slStr="<select id='positions' onchange='onClickSelect()'>";
-				//document.write("<select id='positions' onchange='onClickSelect()'>");
-				for (var i=0; i <limitOfRows; i++)
+				for (var i=0; i <arrCat.length; i++)
 				{
-				positions++;
-				slStr+="<option>" + positions + "</option>";
-				}
-                slStr+="</select>"
-               document.getElementById('select').innerHTML = slStr;
-			}*/
-			
-			///Generate options
-			function selectOptCat(){
-				var limitOfRows=16;
-				var positions = 0;
-				var slStr="<select id='positions' onchange='onClickSelect()'>";
-				//document.write("<select id='positions' onchange='onClickSelect()'>");
-				for (var i=0; i <limitOfRows; i++)
-				{
-				positions++;
-				slStr+="<option>" + positions + "</option>";
+				slStr+="<option>" + arrCat[i] + "</option>";
 				}
                 slStr+="</select>"
                document.getElementById('selectCat').innerHTML = slStr;
+			}
+			
+			function selectOptMan(arrMan){
+				var slStr="<select id='positions' onchange='onClickSelect()'>";
+				for (var i=0; i <arrMan.length; i++)
+				{
+				slStr+="<option>" + arrMan[i] + "</option>";
+				}
+                slStr+="</select>"
+               document.getElementById('selectMan').innerHTML = slStr;
 			}
             // a function to generate table
             function gnerateTable(arrProd) {
@@ -51,9 +45,9 @@ function page_onload(){
 				var keys=Object.keys(arrProd[i]);
 				   tr += '<tr>'; 
 				   for(var j = 0; j < keys.length; j++){
-                    tr +=  '<td>'+arrProd[i][keys[j]]+ '</td>';// + arrProd[keys] + '</td><td>' + arrProd[i] + '</td></tr>';
+                    tr +=  '<td>'+arrProd[i][keys[j]];
 					}
-					tr+='</tr>';
+					tr+= '</td>'+'<td><input type="button" id="delPOIbutton" value="Delete" onclick="deleteRow(this)"/></td></tr>';
 					//tr +=;
 					document.getElementById('tbody').innerHTML = tr;
                 }
